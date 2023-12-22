cf40cbbf31b6c31ce07a666ac5b738a810c131b9
Jennifer Aube <62511019+jaube05@users.noreply.github.com>
2023-12-22 08:47:26 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import NYEWheel from "react-wheel-of-prizes";
 
-function App() {
+export default function App() {
+  const segments = [
+    "80's",
+    "90's",
+    "2000's",
+    "Boy bands",
+    "Romance",
+    "Movies",
+    "Random"
+  ];
+  const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F", "#E79B2E", "#EB18E3"];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>New Year's Eve Music Wheel</h1>
+      <div>
+        <NYEWheel
+          segments={segments}
+          segColors={segColors}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="white"
+          contrastColor="black"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={250}
+          upDuration={300}
+          downDuration={900}
+          fontFamily="Arial"
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
